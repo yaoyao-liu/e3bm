67f74231a36c03e75c70d7a9e8890a03b8e799c8
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2020-03-10 20:47:06 +0100

    Update download.sh

--- data/download.sh
+++ data/download.sh
@@ -5,6 +5,14 @@ rm Mini-ImageNet.zip
 rm -r Mini-ImageNet/train_val Mini-ImageNet/train_test
 mv  Mini-ImageNet/train_train Mini-ImageNet/train 
 
+wget https://www.dropbox.com/s/2hqpf8cqansm1n7/val1000Episode_5_way_5_shot.json?dl=0
+mv val1000Episode_5_way_5_shot.json?dl=0 val1000Episode_5_way_5_shot.json
+mv val1000Episode_5_way_5_shot.json Mini-ImageNet/
+
+wget https://www.dropbox.com/s/0n99mf5ylh4yefi/val1000Episode_5_way_1_shot.json?dl=0
+mv val1000Episode_5_way_1_shot.json?dl=0 val1000Episode_5_way_1_shot.json
+mv val1000Episode_5_way_1_shot.json Mini-ImageNet/
+
 wget https://www.dropbox.com/s/t36y8ng47wlcxw0/netFeatBest64.653.pth?dl=0
 mv netFeatBest64.653.pth?dl=0 netFeatBest64.653.pth
 mkdir ../ckpts
