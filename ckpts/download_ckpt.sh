d0dc8538a9637f21d32a82a2dcb43dbb0c8acaff
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2020-03-11 18:45:37 +0100

    update

--- ckpts/download_ckpt.sh
+++ ckpts/download_ckpt.sh
@@ -3,5 +3,5 @@ mkdir ckpts
 mkdir ckpts/miniImageNet
 mv netFeatBest.pth ckpts/miniImageNet/
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1M7zZRI65TTUF9f3_EFuU_Eo_SxDoT3Cj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1M7zZRI65TTUF9f3_EFuU_Eo_SxDoT3Cj" -O e3bm_ckpt.pth && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1vEFTrtcShLQcm8AqEf-a6EifOXY6LM_5' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1vEFTrtcShLQcm8AqEf-a6EifOXY6LM_5" -O e3bm_ckpt.pth && rm -rf /tmp/cookies.txt
 mv e3bm_ckpt.pth ckpts/miniImageNet/
