d7faa23c3d9622db3e1e9642fdbc322762bba036
Yaoyao Liu <yaoyao.liu@mpi-inf.mpg.de>
2020-07-10 18:41:31 +0200

    Update scripts

--- scripts/download_tieredimagenet.sh
+++ scripts/download_tieredimagenet.sh
@@ -1,4 +1,4 @@
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1bAcWiCO8BeFa0Brh6qs4ybPN15Apq5B8' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1bAcWiCO8BeFa0Brh6qs4ybPN15Apq5B8" -O tiered_imagenet.tar && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1T-4NVTSa5T6CXKSRbymYLnWp_OrtF-mo' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1T-4NVTSa5T6CXKSRbymYLnWp_OrtF-mo" -O tiered_imagenet.tar && rm -rf /tmp/cookies.txt
 tar xvf tiered_imagenet.tar
 rm tiered_imagenet.tar
 mv tiered_imagenet ./data/
